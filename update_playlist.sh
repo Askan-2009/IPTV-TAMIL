8575d15919f91584223b1cc1d6cdd8e3cbccf0d4
Askan-2009 <akilaskan@gmail.com>
2024-06-26 18:11:44 +0530

    Delete update_playlist.sh

--- update_playlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-YOUTUBE_URL="https://www.youtube.com/live/jueJiFeTjR4?si=451FSd-hovbk_8mi"
-M3U8_URL=$(youtube-dl -g -f hls "$YOUTUBE_URL")
-
-echo "#EXTM3U" > playlist.m3u8
-echo "#EXTINF:-1,Live Stream" >> playlist.m3u8
-echo "$M3U8_URL" >> playlist.m3u8
